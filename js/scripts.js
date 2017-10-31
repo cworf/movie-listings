41048481beae2af13aeb95ece7e5559bfca8ee70
Colin Worf and Lena Ha <colin@>
2017-10-31 15:59:35 -0700

    finish code to calculate price

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,16 @@
-function Ticket(movieName, yourAge, showTime){
-  this.movie = movieName;
-  this.age = yourAge;
-  this.showing = showTime;
+function Ticket(movieNameCost, yourAgeCost, showTimeCost, name){
+  this.movieCost = movieNameCost;
+  this.ageCost = yourAgeCost;
+  this.showTimeCost = showTimeCost;
+  this.movieName = name;
+}
+
+Ticket.prototype.movieInformation = function(){
+  return
+}
+
+Ticket.prototype.costCalculation = function(){
+  return this.movieCost + this.ageCost + this.showTimeCost;
 }
 
 $(function(){
@@ -11,11 +20,23 @@ $(function(){
 
   $('.movie-list').submit(function(event){
     event.preventDefault();
-    var movieName = $('input:radio[name=movie]:checked').val();
-    var yourAge = parseInt($('#age'))
-    var showTime = parseInt($('select option:checked').val())
-    console.log(showTime)
+    var movieName = $('input:radio[name=movie]:checked').text();
+    var movieCost = parseInt($('input:radio[name=movie]:checked').val());
+    var ageCost = parseInt($('select[name=ages] option:checked').val());
+    var yourAge = $('select[name=ages] option:checked').text();
+    var showTimeCost = parseInt($('select[name=showings] option:checked').val());
+    var showTime = $('select option:checked').text();
 
-    alert()
+    var customerInfo = new Ticket(movieCost, ageCost, showTimeCost);
+
+    if (yourAge === "Children") {
+      var price = 5;
+    } else {
+      var price = customerInfo.costCalculation();
+    }
+
+    $('.results').show();
+    $('#price').text(price);
+
   });
 });
